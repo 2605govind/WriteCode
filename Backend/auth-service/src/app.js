5acf38b2a85ab91cbc713a6ef97d2b1539bf3267
Govind Suthar <118961773+2605govind@users.noreply.github.com>
2025-07-27 15:25:16 +0530

    Update app.js

--- Backend/auth-service/src/app.js
+++ Backend/auth-service/src/app.js
@@ -10,12 +10,12 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors({
-//   origin: [process.env.FRONTEND_URL, process.env.PROBLEMSERVICE],
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, process.env.PROBLEMSERVICE],
+  credentials: true,
+}));
 
-app.use(cors());
+// app.use(cors());
 
 app.use(cookieParser());
 
